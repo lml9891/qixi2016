665fc21f95ed13343c859975e70656838df6384c
李梦龙 <649076408@qq.com>
2016-07-26 00:17:21 +0800

    fff

--- js/index.js
+++ js/index.js
@@ -228,7 +228,7 @@ function orientationListener(evt) {
 	beta = beta.toFixed(1);
 	alpha = alpha.toFixed(1);
 	if (this._lastGamma != gamma || this._lastBeta != beta) {
-    if (iSpeed<5) {
+    if (iSpeed<3) {
       iSpeed+=1;
     }
 		this._lastGamma = gamma;
